3b2bfd53c117ea993ceb512b6487111a63f7844a
khazaddoom <31121986g@gmail.com>
2024-01-05 17:44:36 +0530

    Tailwind replacement: initial setup

---
+++ css-challenge-final-tailwind-2/tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.{html,vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'gunmetal': {
+          700: '#2E323F'
+        },
+        'arsenic': {
+          700: '#3B4050'
+        },
+        'Sonic Silver': {
+          700: '#7F7B6F'
+        },
+        'grullo': {
+          700: '#A59678'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
+
